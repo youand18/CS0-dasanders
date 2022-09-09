8bfb7f27e22d09c62005cd30f95baf2bf0f1c314
youand18 <78105077+youand18@users.noreply.github.com>
2022-09-09 16:31:01 -0600

    Final Submission
    A little late, for some reason I thought this was due on monday sorry!

---
+++ assignments/stdio.py
@@ -0,0 +1,82 @@
+#David Sanders Standard I/O Program
+
+name = input("Hey there, enter your name please: ")
+print("Well hello, " + name + "!")
+print("The hangman game is under construction, maybe you’ll get to play it in a few weeks…")
+print("This is what various stages of the hangman game will look like…")
+print("""
+|__________ 						
+|/      | 
+| 	    
+|       
+| 	
+|
+| 				              
+------------- 	   
+""")
+input("[Enter to Continue]")
+print("""
+|__________ 						
+|/      | 
+|       O
+|      
+| 	
+|
+| 				              
+------------- 	   
+""")
+input("[Enter to Continue]")
+print("""
+|__________ 						
+|/      | 
+|       O
+|       |
+| 	
+|
+| 				              
+------------- 	   
+""")
+input("[Enter to Continue]")
+print("""
+|__________ 						
+|/      | 
+|       O
+|      /|
+| 	
+|
+| 				              
+------------- 	   
+""")
+input("[Enter to Continue]")
+print("""
+|__________ 						
+|/      | 
+|       O
+|      /|\\
+| 
+|
+| 				              
+------------- 	   
+""")
+input("[Enter to Continue]")
+print("""
+|__________ 						
+|/      | 
+|       O
+|      /|\\
+|      / 
+|
+| 				              
+------------- 	   
+""")
+input("[Enter to Continue]")
+print("""
+|__________ 						
+|/      | 
+|       O
+|      /|\\
+|      / \\
+|
+| 				              
+------------- 	   
+""")
