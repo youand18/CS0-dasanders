ec6e7f79a841acbee99424777f821909f540cfc8
youand18 <78105077+youand18@users.noreply.github.com>
2022-10-03 15:58:34 -0600

    final submission

--- assignments/conditionals/conditionals.py
+++ assignments/conditionals/conditionals.py
@@ -68,18 +68,25 @@ def test():
     assert largest(num1,num2,num3,num4,num5) == 10.2
     assert smallest(num1,num2,num3,num4,num5) == 1.5   
 
-def main():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    num3 = float(input("Enter third number: "))
-    num4 = float(input("Enter fourth number: "))
-    num5 = float(input("Enter fifth number: "))
-    print("sum of 5: " + str(sum5(num1,num2,num3,num4,num5)))
 
-    print("product of 5: " + str(product5(num1,num2,num3,num4,num5)))
-    print("average of 5: " + str(average5(num1,num2,num3,num4,num5)))
-    print("largest: " + str(largest(num1,num2,num3,num4,num5)))
-    print("smallest: " + str(smallest(num1,num2,num3,num4,num5)))
-    test()
+
+def main():
+    looping = True
+    while (looping):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        num3 = float(input("Enter third number: "))
+        num4 = float(input("Enter fourth number: "))
+        num5 = float(input("Enter fifth number: "))
+        print("sum of 5: " + str(sum5(num1,num2,num3,num4,num5)))
+
+        print("product of 5: " + str(product5(num1,num2,num3,num4,num5)))
+        print("average of 5: " + str(average5(num1,num2,num3,num4,num5)))
+        print("largest: " + str(largest(num1,num2,num3,num4,num5)))
+        print("smallest: " + str(smallest(num1,num2,num3,num4,num5)))
+        check = input("Do you want to continue? Y/N: ")
+        if (check == "N" or check == "n"):
+            looping = False
+    #test()
 
 main()
