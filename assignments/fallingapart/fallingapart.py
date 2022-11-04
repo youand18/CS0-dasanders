6fc82ff34247b1dc7feffc18e0de5071a5528ba4
youand18 <78105077+youand18@users.noreply.github.com>
2022-11-04 12:15:16 -0600

    fixed the sorting error, added functions

--- assignments/fallingapart/fallingapart.py
+++ assignments/fallingapart/fallingapart.py
@@ -1,21 +1,38 @@
 #David Sanders Falling Apart Kattis
+def working(line1,line2):
+    line2 = [ int(item) for item in line2]
+    line2.sort(reverse = True)
+    AliceScore = 0
+    BobScore = 0
+    AliceTurn = True
+    i = 0
+    while i < int(line1): 
+        if (AliceTurn):
+            AliceScore = AliceScore + int(line2[i])
+        else:
+            BobScore = BobScore + int(line2[i])
+        i = i + 1
+        AliceTurn = not(AliceTurn)
+    print(AliceScore, BobScore)
+    return(AliceScore,BobScore)
 
-line1 = input()
-line2 = list(input().split())
+def test():
+    assert working("3", ["1","3","2"]) == (4,2), "error on test 1"
+    assert working("1", ["100"]) == (100,0), "error on test 2"
+    assert working("5", ["1","10","2","3","4"]) == (14, 6), "error on test 3"
+    print("testing passed.")
+    main()
 
 
-line2.sort()
 
-AliceScore = 0
-BobScore = 0
-AliceTurn = True
-i = 0
-while i < int(line1): 
-    if (AliceTurn):
-        AliceScore = AliceScore + int(line2[i])
-    else:
-        BobScore = BobScore + int(line2[i])
-    i = i + 1
-    AliceTurn = not(AliceTurn)
+def main():
+    line1 = input()
+    if (line1 == "TEST"):
+        line2 = test()
+    line2 = list(input().split())
+    working(line1,line2)
 
-print(str(AliceScore) + " " + str(BobScore))
+
+
+    
+main()
