1e2e6a24987ebcd3929c0d1c4527ec8e2388365c
youand18 <78105077+youand18@users.noreply.github.com>
2022-11-04 11:48:54 -0600

    initial trial

--- assignments/fallingapart/fallingapart.py
+++ assignments/fallingapart/fallingapart.py
@@ -5,7 +5,14 @@ line2 = list(input().split())
 
 line2.sort()
 
+AliceScore = 0
+BobScore = 0
 AliceTurn = True
 i = 0
 while i < int(line1): 
-    
+    if (AliceTurn):
+        AliceScore = AliceScore + line2[i]
+    else:
+        BobScore = BobScore + line2[i]    
+
+print(AliceScore + " " + BobScore)
