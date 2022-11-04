2f2287a56c6accf4a148c397954e328017a26cc8
youand18 <78105077+youand18@users.noreply.github.com>
2022-11-04 11:51:49 -0600

    fixed typecast errors

--- assignments/fallingapart/fallingapart.py
+++ assignments/fallingapart/fallingapart.py
@@ -3,6 +3,7 @@
 line1 = input()
 line2 = list(input().split())
 
+
 line2.sort()
 
 AliceScore = 0
@@ -11,8 +12,9 @@ AliceTurn = True
 i = 0
 while i < int(line1): 
     if (AliceTurn):
-        AliceScore = AliceScore + line2[i]
+        AliceScore = AliceScore + int(line2[i])
     else:
-        BobScore = BobScore + line2[i]    
+        BobScore = BobScore + int(line2[i])
+    i = i + 1
 
-print(AliceScore + " " + BobScore)
+print(str(AliceScore) + " " + str(BobScore))
