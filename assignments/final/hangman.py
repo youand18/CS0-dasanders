e0cf3666106090fcbee557c24e6181b2f5757399
youand18 <78105077+youand18@users.noreply.github.com>
2022-12-11 23:10:48 -0700

    Loss State check

--- assignments/final/hangman.py
+++ assignments/final/hangman.py
@@ -1,3 +1,16 @@
+import time
+from os import system, name
+
+def clear():
+  
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
 def hangmanStates(stateInt):
     if(stateInt == 0):
         print("""
@@ -87,3 +100,31 @@ def hangmanGuessLine(correctStringChecks, correctString):
             printString = printString + "_"
         i = i + 1
     print(printString)
+
+def hangman(stateInt, correctString, correctStringChecks):
+    clear()
+    going = True
+    lettersGuessed = []
+
+    while (going):
+        hangmanStates(stateInt)
+        hangmanGuessLine(correctStringChecks, correctString)
+        letterGuess = input("Enter Guess: ")
+        if(letterGuess.isalpha()):
+            lettersGuessed.append(letterGuess)
+            guessString = "Guesses: "
+            for letter in letterGuessed:
+                guessString = guessString + (letter + ",")
+            print(guessString)
+            changed = False
+            i = 0
+            for char in correctString:
+                if (letterGuess == char):
+                    correctStringChecks[i] = True
+                    changed = True
+                i = i + 1
+            if (changed == False):
+                stateInt = stateInt + 1
+                if (stateInt >= 6):
+                    hangmanStates(6)
+                    print("You Lose! The correct word was %s"%(correctString) + ".")
